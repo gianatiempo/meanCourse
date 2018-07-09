b7a6e858f707b96b5edb3f6e179e55767dbb18ac
Ari Giana <gianatiempo@gmail.com>
2018-07-08 23:49:18 -0300

    setup for integrated deploy

--- backend/app.js
+++ backend/app.js
@@ -18,23 +18,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 if (process.env.NODE_ENV !== 'production') {
   app.use("/images", express.static(path.join("backend/images")));
 } else {
-  app.use("/images", express.static(path.join("images")));
+  app.use("/images", express.static(path.join(__dirname, "images")));
+  app.use("/", express.static(path.join(__dirname, "front")));
 }
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+//   );
+//   next();
+// });
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
+app.use((req, res, next) => {
+  res.sendfile(__dirname, path.join('front', 'index.html'));
+})
 
 module.exports = app;
